58f5ffc045718813f7c9ff75766cb0a4e3ba3da0
Liz Ainslie <lizainslie16@gmail.com>
2023-11-23 08:05:09 -0700

    chore(all): prep for major changes

--- crates/oxide_plugin/src/lib.rs
+++ crates/oxide_plugin/src/lib.rs
@@ -2,7 +2,7 @@ use oxide_core::Package;
 use oxide_core::Repository;
 use oxide_core::NSID;
 
-trait Plugin<T : Package> {
+trait DaemonPlugin<T : Package> {
     const IDENTIFIER: NSID;
 
     fn register_repository(&self, name: String, repo: Box<dyn Repository<T>>);
