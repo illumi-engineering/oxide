b65e98bc0b249859f1b1048b535a5c5038cc0c22
Liz Ainslie <lizainslie16@gmail.com>
2023-12-28 19:07:33 -0700

    chore(all): remove useless plugin code, prepare gradle root

--- crates/oxide_plugin/src/project.rs
+++
@@ -1,9 +0,0 @@
-use oxide_project::OxideProject;
-
-pub struct DaemonPluginProjectContext {
-    pub project: Box<OxideProject>
-}
-
-pub trait DaemonPluginProjectScope {
-    fn download_deps(&self, ctx: DaemonPluginProjectContext) -> Result<(), &str>;
-}
